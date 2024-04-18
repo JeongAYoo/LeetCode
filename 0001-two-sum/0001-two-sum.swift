13b0e4b2b8790e9f8ade0a4cf50ab395687b7110
Jade Yoo <jeonga038@gmail.com>
2024-04-18 14:54:54 +0900

    Time: 19 ms (83.85%), Space: 16.2 MB (52.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -5,9 +5,8 @@ class Solution {
 
         // get both index and value
         for (index, num) in nums.enumerated() {
-            // 딕셔너리에 값이 이미 존재한다면
-            if let firstIndex = dict[num] {
-                return [firstIndex, index]
+            if let diff = dict[num] {
+                return [diff, index]
             } else {
                 // 합이 target이 될 숫자 == target - 현재 숫자
                 dict[target - num] = index
