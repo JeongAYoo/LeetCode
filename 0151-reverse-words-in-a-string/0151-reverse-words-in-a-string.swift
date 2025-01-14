52a27d820254d69bdf39e39a222df28139e0794a
Jade Yoo <jeonga038@gmail.com>
2025-01-14 19:09:51 +0900

    Time: 6 ms (48.07%), Space: 20.1 MB (5.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.swift
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.swift
@@ -1,6 +1,6 @@
 class Solution {
     func reverseWords(_ s: String) -> String {
         var str = s
-        return str.split(separator: " ").reversed().map { String($0) }.joined(separator: " ")
+        return str.split(separator: " ").reversed().joined(separator: " ")
     }
 }
