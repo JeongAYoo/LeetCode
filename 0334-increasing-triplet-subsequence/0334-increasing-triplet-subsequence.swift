98d1545d56d0ec4375c5f8bde22735ac516f530a
Jade Yoo <jeonga038@gmail.com>
2025-07-09 23:29:39 +0900

    Time: 2 ms (33.86%), Space: 34.2 MB (19.69%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func increasingTriplet(_ nums: [Int]) -> Bool {
+        var min = Int.max
+        var mid = Int.max
+
+        for num in nums where nums.count >= 3 {
+            if num < min {
+                min = num
+            } else if num > min && num < mid {
+                mid = num
+            } else if num > mid {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
